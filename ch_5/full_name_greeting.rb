6f63c98d71cf8e123cb235f34df312b9696dd0c8
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2018-03-29 23:07:07 -0500

    Finished full_name_greeting program.

---
+++ ch_5/full_name_greeting.rb
@@ -0,0 +1,17 @@
+# Learn To Program - Chris Pine - Ch.5 exercises
+
+# This program will ask a person for their first, middle and last names and then greet the person.
+
+# get the persons first, middle, and last name
+puts "What is your first name?"
+first_name = gets.chomp
+
+puts "What is your middle name?"
+middle_name = gets.chomp
+
+puts "What is your last name?"
+last_name = gets.chomp
+
+# greet the person
+puts "Nice to meet you #{first_name} #{middle_name} #{last_name}!"
+
